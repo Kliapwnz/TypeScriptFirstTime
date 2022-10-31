f1e09ceba5972a1ea7e14b00f8c77d09fc6fa19b
Kliapwnz <kliasanik@gmail.com>
2022-10-31 12:15:43 +0300

    StarPropsType

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -75,8 +75,11 @@ export function Rating(props: RatingPropsType) {
 
 }
 
+type StarPropsType = {
+    selected : boolean
+}
 
-function Star(props: any) {
+function Star(props: StarPropsType) {
     console.log("Star rendering")
 
     if (props.selected === true) {
